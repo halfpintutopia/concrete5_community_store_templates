5bf51b482a1267fd42c8f9b2159f8900dc0e375d
Ana Richardson <siri.richardson@gmail.com>
2020-06-02 09:00:11 +0200

    Initial package set-up

--- blocks/community_product_list/add.php
+++
@@ -1,3 +0,0 @@
-<?php
-defined('C5_EXECUTE') or die("Access Denied.");
-$this->inc('form.php', ['showAddToCart' => true, 'showPageLink' => true, 'showDescription' => true, 'showPrice' => true, 'showName' => true, 'enableExternalFiltering' => true]);
