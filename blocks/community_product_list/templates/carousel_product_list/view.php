4150de1457854e2cea77332712014943afe938ab
Ana Richardson <siri.richardson@gmail.com>
2020-06-02 15:11:43 +0200

    Carousel product list - one row

--- blocks/community_product_list/templates/carousel_product_list/view.php
+++ blocks/community_product_list/templates/carousel_product_list/view.php
@@ -31,6 +31,72 @@ if ($productsPerRow == 6) {
 
 ?>
 
+<style>
+    @media (min-width: 768px) {
+
+        /* show 3 items */
+        .carousel-inner .active,
+        .carousel-inner .active+.carousel-item,
+        .carousel-inner .active+.carousel-item+.carousel-item {
+            display: block;
+        }
+
+        .carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left),
+        .carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left)+.carousel-item,
+        .carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left)+.carousel-item+.carousel-item {
+            transition: none;
+        }
+
+        .carousel-inner .carousel-item-next,
+        .carousel-inner .carousel-item-prev {
+            position: relative;
+            transform: translate3d(0, 0, 0);
+        }
+
+        .carousel-inner .active.carousel-item+.carousel-item+.carousel-item+.carousel-item {
+            position: absolute;
+            top: 0;
+            right: -33.3333%;
+            z-index: -1;
+            display: block;
+            visibility: visible;
+        }
+
+        /* left or forward direction */
+        .active.carousel-item-left+.carousel-item-next.carousel-item-left,
+        .carousel-item-next.carousel-item-left+.carousel-item,
+        .carousel-item-next.carousel-item-left+.carousel-item+.carousel-item,
+        .carousel-item-next.carousel-item-left+.carousel-item+.carousel-item+.carousel-item {
+            position: relative;
+            transform: translate3d(-100%, 0, 0);
+            visibility: visible;
+        }
+
+        /* farthest right hidden item must be abso position for animations */
+        .carousel-inner .carousel-item-prev.carousel-item-right {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            display: block;
+            visibility: visible;
+        }
+
+        /* right or prev direction */
+        .active.carousel-item-right+.carousel-item-prev.carousel-item-right,
+        .carousel-item-prev.carousel-item-right+.carousel-item,
+        .carousel-item-prev.carousel-item-right+.carousel-item+.carousel-item,
+        .carousel-item-prev.carousel-item-right+.carousel-item+.carousel-item+.carousel-item {
+            position: relative;
+            transform: translate3d(100%, 0, 0);
+            visibility: visible;
+            display: block;
+            visibility: visible;
+        }
+
+    }
+</style>
+
 <div class="carousel-product-list-wrapper">
     <div class="container">
         <div class="store-product-list-block">
@@ -408,6 +474,30 @@ if ($productsPerRow == 6) {
                     </a>
 
                 </div> <!-- .carousel -->
+
+                <script>
+                    jQuery(function($) {
+                        $('#productistCarousel').on('slide.bs.carousel', function(e) {
+
+                            var $e = $(e.relatedTarget);
+                            var idx = $e.index();
+                            var itemsPerSlide = 4;
+                            var totalItems = $('.carousel-item').length;
+
+                            if (idx >= totalItems - (itemsPerSlide - 1)) {
+                                var it = itemsPerSlide - (totalItems - idx);
+                                for (var i = 0; i < it; i++) {
+                                    // append slides to end
+                                    if (e.direction == "left") {
+                                        $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                                    } else {
+                                        $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                                    }
+                                }
+                            }
+                        });
+                    });
+                </script>
 
                 <?php if ($showPagination) { ?>
                     <?php if ($paginator->getTotalPages() > 1) { ?>
